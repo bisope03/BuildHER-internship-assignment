7245054c05b21933168fb9de02bde93c57f4afd2
Bishakha Gope <84770275+bisope03@users.noreply.github.com>
2023-03-19 14:20:05 +0530

    Update Easy.cpp

--- Easy.cpp
+++ Easy.cpp
@@ -1,58 +1,30 @@
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
 /*
-Given an integer array nums where the elements are sorted in ascending order, convert it to a 
-height-balanced binary search tree.
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+A word is a maximal 
+substring consisting of non-space characters only.
 
-Time complexity: O(n) where n is the number of nodes
-Space complexity: O(h) where h is the height of the tree
+Time complexity: O(n) where n is the length of the string
+Space complexity: O(1) 
+
 */
 
-struct TreeNode{
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-TreeNode* helper(vector<int>& nums, int low, int high){
-  if(low > high) return NULL;
-
-  int mid = low + (high - low)/2;
-  TreeNode* node = new TreeNode(nums[mid]);
-  node->left = helper(nums, low, mid-1);
-  node->right = helper(nums, mid+1, high);
-  return node;
-}
-
-TreeNode* sortedArraytoBST(vector<int>& nums){
-  if(nums.size()==0) return NULL;
-  TreeNode* head = helper(nums, 0, nums.size()-1);
-  return head;
-}
-
-void preOrder(TreeNode* node){
-  if(node == NULL) 
-    return;
-
-  cout << node->val << " ";
-  preOrder(node->left);
-  preOrder(node->right);
-}
-
 int main(){
-  int n; 
-  cout << "Enter the number of nodes: ";
-  cin >> n;
-  cout << "Enter the nodes: ";
-  vector<int> nums(n);
-  for(int i=0; i<n; i++){
-    cin >> nums[i];
-  }
-  TreeNode* head = sortedArraytoBST(nums);
-  cout << "Preorder Traversal of constructed BST" << endl;
-  preOrder(head);
-}
+    // string input
+    string s;
+    getline(cin, s);
 
+    // solution
+    int count=0;
+    for(int i=s.length()-1; i>=0; i--){
+        if(s[i]==' ' && count>0) break;
+        else if(s[i]!=' '){
+            count++;    
+        }
+    }
+    cout << count;
+    return 0;
+}
